e1aeacc78048bfb858a51fcdb6d95d2157a3593f
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-05-11 00:34:32 +0900

    add drawer

--- lib/presentation/views/widgets/common/menu_drawer.dart
+++ lib/presentation/views/widgets/common/menu_drawer.dart
@@ -1,30 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../../views.dart';
+
 class MenuDrawer extends StatelessWidget {
   MenuDrawer({
     super.key,
   });
 
-  final List<String> _menuItems = <String>[
-    'About': ,
-    'Contact',
-    'Settings',
-    'Sign Out',
-  ];
+  final _menuItems = {
+    'About': const AboutPage(),
+    'Settings': const SettingPage()
+  };
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        children: _menuItems
-            .map((item) => ListTile(
-          onTap: () {
-
-          },
-          title: Text(item),
-        ))
-            .toList(),
+        padding: EdgeInsets.zero,
+        children: _menuItems.keys.map((item) =>
+          ListTile(
+            title: Text(item),
+            onTap: () {
+              Scaffold.of(context).openEndDrawer();
+              _navigateTo(context, _menuItems[item]!);
+            },
+          ),
+        ).toList(),
       ),
     );
   }
+
+  void _navigateTo(BuildContext context, StatefulWidget page){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => page,
+      ),
+    );
+  }
+
 }
