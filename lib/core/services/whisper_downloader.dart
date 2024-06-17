877eafb7899aa59c601141448ebfdbb4f09302c9
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-06-17 18:30:59 +0900

    add base inferencer & downloader service class

---
+++ lib/core/services/whisper_downloader.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:whisper_flutter_new/whisper_flutter_new.dart';
+import 'package:path/path.dart' as path;
+import "package:path_provider/path_provider.dart";
+
+const defaultDownloadHost = "https://huggingface.co/ggerganov/whisper.cpp/resolve/main";
+
+class WhisperDownloader {
+
+  WhisperDownloader({
+   required this.httpClient
+  });
+
+  final HttpClient httpClient;
+
+  Future<String> downloadModel({
+    required WhisperModel modelType,
+    String? modelDir,
+    String? downloadHost,
+    Function(double)? onProgress,
+  }) async {
+    if (downloadHost == null || downloadHost.isEmpty) {
+      downloadHost = defaultDownloadHost;
+    }
+
+    if (modelDir == null || modelDir.isEmpty){
+      final tempDir = await getTemporaryDirectory();
+      modelDir = path.join(tempDir.path, "whisper_offline", "models");
+    }
+
+    if (await isModelExist(modelType: modelType, modelDir: modelDir)){
+      return path.join(modelDir, "ggml-${modelType.modelName}.bin");
+    }
+
+    final Uri modelUri = Uri.parse(
+      "$downloadHost/ggml-${modelType.modelName}.bin",
+    );
+
+    final request = await httpClient.getUrl(
+      modelUri,
+    );
+
+    final response = await request.close();
+
+    final file = File(path.join(modelDir, "ggml-${modelType.modelName}.bin"));
+    final raf = file.openSync(mode: FileMode.write);
+
+    final contentLength = response.headers.contentLength;
+    int bytesRead = 0;
+    await for (var chunk in response) {
+      raf.writeFromSync(chunk);
+      bytesRead += chunk.length;
+      if (onProgress != null) {
+        onProgress(bytesRead / contentLength);
+      }
+    }
+
+    await raf.close();
+
+    if (kDebugMode) {
+      debugPrint("Download Down . Path = ${file.path}");
+    }
+    return file.path;
+  }
+
+  Future<bool> isModelExist({
+    required WhisperModel modelType,
+    String? modelDir,
+  }) async {
+    if (modelDir == null || modelDir.isEmpty){
+      final tempDir = await getTemporaryDirectory();
+      modelDir = path.join(tempDir.path, "whisper_offline", "models");
+    }
+    final modelPath = path.join(modelDir, "ggml-${modelType.modelName}.bin");
+    final modelFile = File(modelPath);
+
+    return await modelFile.exists();
+  }
+
+}
--- lib/core/services/whisper_inferencer.dart
+++ lib/core/services/whisper_inferencer.dart
@@ -1,29 +1,100 @@
 import "dart:io";
 import "package:flutter/material.dart";
-import "package:whisper_dart/scheme/scheme.dart";
-import "package:whisper_dart/whisper_dart.dart";
+import "package:path_provider/path_provider.dart";
+import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
+import 'package:whisper_flutter_new/download_model.dart';
+import 'package:whisper_flutter_new/whisper_flutter_new.dart';
+import 'package:path/path.dart' as path;
 
 class WhisperInferencer{
 
   WhisperInferencer({
-    required this.model
+    this.modelType,
+    this.modelDir,
   });
 
-  Whisper model;
+  final FlutterFFmpeg ffmpeg = FlutterFFmpeg();
+  final availableModels = WhisperModel.values.map((model) => model.modelName).toList();
+  String? modelDir;
+  WhisperModel? modelType;
+  Whisper? model;
 
-  void transcribe({
-    required String audioPath,
-    String modelPath="large-v2"
+  Future<void> init() async {
+    if (modelDir==null){
+      final tempDir = await getTemporaryDirectory();
+      modelDir = path.join(tempDir.path, "whisper_offline", "models");
+    }
+
+    modelType ??= WhisperModel.largeV2;
+
+    _initModel(newModelType: modelType!);
+  }
+
+  Future<String> transcribe({
+    required String filePath,
+    WhisperModel modelType=WhisperModel.largeV2,
+    bool isTranslate=false,
+    bool isNoTimestamps=false,
   }) async {
-    Transcribe transcribeAnyAudio = await model.transcribe(
-      audio: WhisperAudioconvert.convert(
-        audioInput: File("./path_audio_any_format"),
-        audioOutput: File("./path_audio_convert.wav"),
-      ).path,
-      model: "./path_model_whisper_bin",
-      language: "id", // language
+    _initModel(newModelType: modelType);
+    
+    final preprocessedFilePath = await _preProcess(filePath: filePath);
+    if (preprocessedFilePath==null){
+      throw("Error pre-process input File");
+    }
+
+    final WhisperTranscribeResponse transcription = await model!.transcribe(
+      transcribeRequest: TranscribeRequest(
+        audio: preprocessedFilePath,
+        isTranslate: false,
+        isNoTimestamps: false,
+        splitOnWord: false,
+        isVerbose: true,
+        diarize: false
+      ),
     );
 
-    debugPrint("$transcribeAnyAudio");
+    return "${transcription.toJson()}";
+  }
+
+  void _initModel({
+    required WhisperModel newModelType,
+  }) {
+    if (newModelType != modelType || model == null){
+      model = Whisper(
+        model: newModelType,
+        modelDir: modelDir!,
+      );
+      modelType = newModelType;
+    }
+  }
+
+  Future<String?> _preProcess({
+    required String filePath,
+  }) async {
+    String tempDir = (await getTemporaryDirectory()).path;
+    tempDir = path.join(tempDir, "whisper_offline");
+    if (!(await Directory(tempDir).exists())) {
+      await Directory(tempDir).create(recursive: true);
+    }
+
+    final tempFilePath = path.join(tempDir, "preprocessed_temp.wav");
+
+    try {
+      int result = await ffmpeg.execute(
+          '-y -i "$filePath" -ar 16000 -ac 1 -c:a pcm_s16le "$tempFilePath"'
+      );
+
+      if (result == 0) {
+        debugPrint('Conversion successful');
+      } else {
+        debugPrint('Error during conversion: error code is $result');
+      }
+
+      return tempFilePath;
+    } catch (e) {
+      debugPrint('Error: $e');
+      return null;
+    }
   }
 }
